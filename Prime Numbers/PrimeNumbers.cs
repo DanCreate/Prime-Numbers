0e877b1259658020b83ea95a59df18d85d77f2a5
Daniel Borges Gamboa <dborges30324@ufide.ac.cr>
2023-04-21 13:54:49 -0600

    Add project files.

---
+++ Prime Numbers/PrimeNumbers.cs
@@ -0,0 +1,45 @@
+﻿
+
+class PrimeNumbers
+{
+
+    static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("Enter a number");
+        number = int.Parse(Console.ReadLine());
+        
+        var primenumber = PrimeEvaluation(number);
+
+        if (primenumber == true)
+        {
+
+            Console.WriteLine(number + " is a prime number");
+
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a prime number");
+        }
+
+    }
+
+    public static bool PrimeEvaluation(int number)
+    {
+
+        for (int i = 2; i < number; i++)
+        {
+
+            if (number % i == 0)
+            {
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+}
